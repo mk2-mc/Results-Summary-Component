5181dfb1ea7c9f3eb81d6c7c34d931054ac6753a
Mk.2 <55669745+mk2-mc@users.noreply.github.com>
2023-10-02 02:45:39 +0100

    Update

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const summary = document.querySelectorAll(".summary-box");
-const name = document.querySelectorAll(".summary-name");
-const score = document.querySelectorAll(".summary-score");
-const result = document.querySelectorAll(".result-value");
+const result = document.querySelector(".result-value");
+let scoresCount = 0;
+let scoresTotal = 0;
 
 const getData = async () => {
   const response = await fetch("./data.json");
   const data = await response.json();
   
-  console.log(data);
-  return data;
+  // Summary
+  summary.forEach((item, i) => {
+    const img = document.createElement("img");
+    img.src = data[i].icon;
+    item.insertAdjacentElement("afterbegin", img);
+
+    item.querySelector(".summary-name").innerHTML = data[i].category;
+    item.querySelector(".summary-score").innerHTML = data[i].score;
+    
+    scoresCount ++;
+    scoresTotal += data[i].score;
+  });
+
+  // Result
+  result.innerHTML = Math.floor(scoresTotal / scoresCount);
 }
 
 getData();
